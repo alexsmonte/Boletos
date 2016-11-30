249787f2cf7df824e4fa3a4700db1533c7828944
Alex Monte <alexsmonte@gmail.com>
2016-11-30 10:46:52 -0400

    Implementando interface

--- src/BoletoInterface.php
+++ src/BoletoInterface.php
@@ -6,4 +6,12 @@ namespace Asmpkg\Boleto;
 interface BoletoInterface
 {
 
+    public function codigoCedente($codigoCedente);
+    public function carteira($carteira);
+    public function nossoNumero($nossoNumero);
+    public function valor($valor);
+    public function linhaDigitavel();
+    public function codigoBarras();
+
+
 }
