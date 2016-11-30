e3d3f526bdcea621df83293de8c74c6f5bde7432
Alex Monte <alexsmonte@gmail.com>
2016-11-30 10:45:38 -0400

    caregando a class de boletos "automaticamente"

--- src/Boleto.php
+++ src/Boleto.php
@@ -1,10 +1,61 @@
 <?php
-
-
 namespace Asmpkg\Boleto;
-
+use ReflectionClass;
 
 class Boleto
 {
 
+    const VERSION = '0.1';
+
+    private $banco;
+    private $vencimento;
+    private $valor;
+
+    public function __construct ($banco)
+    {
+        $namespace  =    new ReflectionClass("Asmpkg\\Boleto\\Banco\\".$banco);
+        $this->banco      =    $namespace->newInstance();
+    }
+
+
+    public function codigoCedente($codigoCedente)
+    {
+        $this->banco->codigoCedente($codigoCedente);
+        return $this;
+    }
+
+    public function carteira($carteira)
+    {
+        $this->banco->carteira($carteira);
+        return $this;
+    }
+
+    public function nossoNumero($nossoNumero)
+    {
+        $this->banco->nossoNumero($nossoNumero);
+        return $this;
+    }
+
+    public function valor($valor)
+    {
+        $this->valor    =   $valor;
+        $this->banco->valor($valor);
+        return $this;
+    }
+
+    public function vencimento($vencimento)
+    {
+        $this->vencimento   =   $vencimento;
+        $this->banco->vencimento($vencimento);
+        return $this;
+    }
+
+    public function linhaDigitavel(){
+        return $this->banco->linhaDigitavel();
+    }
+
+    public function codigoBarras()
+    {
+        return $this->banco->codigoBarras();
+    }
 }
