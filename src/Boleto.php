72c970e8efcef0453de0d972f24d2928068a010e
Alex Monte <alexsmonte@gmail.com>
2018-06-22 00:05:23 -0400

    tornando publica algumas variaveis e adicionando o codigo de barras

--- src/Boleto.php
+++ src/Boleto.php
@@ -8,13 +8,13 @@ class Boleto
     const VERSION = '0.1';
 
     private $banco;
-    private $vencimento;
-    private $valor;
+    public $vencimento;
+    public $valor;
 
-    public function __construct ($banco)
+    public function __construct($banco)
     {
-        $namespace      =    new ReflectionClass("Asmpkg\\Boleto\\Banco\\".$banco);
-        $this->banco    =    $namespace->newInstance();
+        $namespace = new ReflectionClass("Asmpkg\\Boleto\\Banco\\" . $banco);
+        $this->banco = $namespace->newInstance();
     }
 
     public function conta($conta)
@@ -49,19 +49,20 @@ class Boleto
 
     public function valor($valor)
     {
-        $this->valor    =   $valor;
+        $this->valor = $valor;
         $this->banco->valor($valor);
         return $this;
     }
 
     public function vencimento($vencimento)
     {
-        $this->vencimento   =   $vencimento;
+        $this->vencimento = $vencimento;
         $this->banco->vencimento($vencimento);
         return $this;
     }
 
-    public function linhaDigitavel(){
+    public function linhaDigitavel()
+    {
         return $this->banco->linhaDigitavel();
     }
 
@@ -69,4 +70,121 @@ class Boleto
     {
         return $this->banco->codigoBarras();
     }
+
+    private function esquerda($entra, $comp)
+    {
+        return substr($entra, 0, $comp);
+    }
+
+    private function direita($entra, $comp)
+    {
+        return substr($entra, strlen($entra) - $comp, $comp);
+    }
+
+    /**
+     * @param $valor
+     *
+     * codigo retirado do phpboleto
+     */
+    public function fBarCode($valor)
+    {
+        $fino = 1;
+        $largo = 3;
+        $altura = 50;
+
+        $barcodes[0] = "00110";
+        $barcodes[1] = "10001";
+        $barcodes[2] = "01001";
+        $barcodes[3] = "11000";
+        $barcodes[4] = "00101";
+        $barcodes[5] = "10100";
+        $barcodes[6] = "01100";
+        $barcodes[7] = "00011";
+        $barcodes[8] = "10010";
+        $barcodes[9] = "01010";
+
+        for ($f1 = 9; $f1 >= 0; $f1--) {
+            for ($f2 = 9; $f2 >= 0; $f2--) {
+                $f = ($f1 * 10) + $f2;
+                $texto = "";
+                for ($i = 1; $i < 6; $i++) {
+                    $texto .= substr($barcodes[$f1], ($i - 1), 1) . substr($barcodes[$f2], ($i - 1), 1);
+                }
+                $barcodes[$f] = $texto;
+            }
+        }
+
+        $codigoBarra[0]["imagem"] = 'p';
+        $codigoBarra[0]["width"] = $fino;
+        $codigoBarra[0]["height"] = $altura;
+
+        $codigoBarra[1]["imagem"] = 'b';
+        $codigoBarra[1]["width"] = $fino;
+        $codigoBarra[1]["height"] = $altura;
+
+        $codigoBarra[2]["imagem"] = 'p';
+        $codigoBarra[2]["width"] = $fino;
+        $codigoBarra[2]["height"] = $altura;
+
+        $codigoBarra[3]["imagem"] = 'b';
+        $codigoBarra[3]["width"] = $fino;
+        $codigoBarra[3]["height"] = $altura;
+
+        $texto = $valor;
+        if ((strlen($texto) % 2) <> 0)
+            $texto = "0" . $texto;
+
+        $contador = 4;
+        while (strlen($texto) > 0)
+        {
+            $i = round($this->esquerda($texto,2));
+            $texto = $this->direita($texto,strlen($texto)-2);
+            $f = $barcodes[$i];
+
+            for($i=1;$i<11;$i+=2)
+            {
+                if (substr($f,($i-1),1) == "0")
+                    $f1 = $fino ;
+                else
+                    $f1 = $largo ;
+
+                $codigoBarra[$contador]["imagem"]   = 'p';
+                $codigoBarra[$contador]["width"]    = $f1;
+                $codigoBarra[$contador]["height"]   = $altura;
+
+                $contador++;
+
+                if (substr($f,$i,1) == "0")
+                    $f2 = $fino ;
+                else
+                    $f2 = $largo ;
+
+                $codigoBarra[$contador]["imagem"]   = 'b';
+                $codigoBarra[$contador]["width"]    = $f2;
+                $codigoBarra[$contador]["height"]   = $altura;
+
+                $contador++;
+            }
+        }
+
+        $codigoBarra[$contador]["imagem"]   = 'p';
+        $codigoBarra[$contador]["width"]    = $largo;
+        $codigoBarra[$contador]["height"]   = $altura;
+
+        $contador++;
+
+        $codigoBarra[$contador]["imagem"]   = 'b';
+        $codigoBarra[$contador]["width"]    = $fino;
+        $codigoBarra[$contador]["height"]   = $altura;
+
+        $contador++;
+
+        $codigoBarra[$contador]["imagem"]   = 'p';
+        $codigoBarra[$contador]["width"]    = 1;
+        $codigoBarra[$contador]["height"]   = $altura;
+
+        return $codigoBarra;
+    }
+
+
 }
