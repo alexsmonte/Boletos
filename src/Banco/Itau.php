9f09fbd40f901fd7ebc9722a03b0fbbe6d09fbea
Alex Monte <alexsmonte@gmail.com>
2018-06-28 11:08:24 -0400

    finalizando boleto itau

--- src/Banco/Itau.php
+++ src/Banco/Itau.php
@@ -9,12 +9,14 @@ class Itau extends Utilitario implements BoletoInterface
     const BANCO =   "341";
     const MOEDA =   9;
 
-    private $nossoNumero        =   null;
+    public $nossoNumero        =   null;
     private $codigoCedente      =   null;
     private $carteira           =   null;
     private $dvCodigoBarras     =   null;
     private $fatorVencimento    =   "0000";
     private $valor;
+    private $agencia;
+    private $conta;
 
     public function __construct()
     {
@@ -25,6 +27,19 @@ class Itau extends Utilitario implements BoletoInterface
     {
         return static::BANCO;
     }
+
+    public function agencia($agencia)
+    {
+        $this->agencia  =   $agencia;
+        return $this;
+    }
+
+    public function conta($conta)
+    {
+        $this->conta  =   $conta;
+        return $this;
+    }
+
 
     public function carteira($carteira)
     {
@@ -51,16 +66,29 @@ class Itau extends Utilitario implements BoletoInterface
 
     public function nossoNumero($nossoNumero)
     {
-        // TODO: Implement nossoNumero() method.
+        $this->nossoNumero  =   str_pad($nossoNumero, 8, "0", STR_PAD_LEFT);
+
+        $numero = $this->agencia.$this->conta.$this->carteira.$nossoNumero;
+        $this->dvDacNossoNumero    =   $this->modulo10($numero);
+        return $this;
     }
 
     public function linhaDigitavel()
     {
-        // TODO: Implement linhaDigitavel() method.
+        return $this->primeiroCampo().$this->segundoCampo().$this->terceiroCampo().$this->quartoCampo().$this->quintoCampo();
     }
+
+
     public function codigoBarras()
     {
-        // TODO: Implement codigoBarras() method.
+        $banco  =   static::BANCO;
+        $moeda  =   static::MOEDA;
+
+        $codigoBarra    =   $banco.$moeda.$this->fatorVencimento.str_pad($this->formatarValor($this->valor), 10, "0", STR_PAD_LEFT).$this->carteira.$this->nossoNumero.$this->dvDacNossoNumero.$this->agencia.$this->conta.$this->modulo10($this->agencia.$this->conta).'000';
+
+        $dacCodigoBarra	=	$this->modulo11($codigoBarra);
+
+        return $banco.$moeda.$dacCodigoBarra.$this->fatorVencimento.str_pad($this->formatarValor($this->valor), 10, "0", STR_PAD_LEFT).$this->carteira.$this->nossoNumero.$this->dvDacNossoNumero.$this->agencia.$this->conta.$this->modulo10($this->agencia.$this->conta).'000';
     }
 
     private function primeiroCampo()
@@ -68,7 +96,45 @@ class Itau extends Utilitario implements BoletoInterface
         $banco  =   static::BANCO;
         $moeda  =   static::MOEDA;
 
-        $primeiroGrupo  =   $banco.$moeda.$this->carteira.substr($this->nossoNumero, 0, 2);
+        $primeiroCampo  =   $banco.$moeda.$this->carteira.substr($this->nossoNumero,0,2);
+        $dacPrimeiroCampo   =   $this->modulo10($primeiroCampo);
+
+        $primeiroCampo  .= $dacPrimeiroCampo;
+
+        return $primeiroCampo;
+    }
+
+    private function segundoCampo()
+    {
+        $segundoCampo	= substr($this->nossoNumero,2,6).$this->dvDacNossoNumero.substr($this->agencia,0, 3);
+
+        $dacSegundoCampo   =   $this->modulo10($segundoCampo);
+
+        return substr($this->nossoNumero,2,6).$this->dvDacNossoNumero.substr($this->agencia,0, 3).$dacSegundoCampo;
+    }
+
+    private function terceiroCampo()
+    {
+        $terceiroCampo	=	substr($this->agencia,3, strlen($this->agencia)-3).$this->conta.$this->modulo10($this->agencia.$this->conta).'000';
+
+        $dacTerceiroCampo =   $this->modulo10($terceiroCampo);
+
+        return $terceiroCampo.$dacTerceiroCampo;
+    }
+
+    private function quartoCampo()
+    {
+        $banco  =   static::BANCO;
+        $moeda  =   static::MOEDA;
+
+        $codigoBarra    =   $banco.$moeda.$this->fatorVencimento.str_pad($this->formatarValor($this->valor), 10, "0", STR_PAD_LEFT).$this->carteira.$this->nossoNumero.$this->dvDacNossoNumero.$this->agencia.$this->conta.$this->modulo10($this->agencia.$this->conta).'000';
+
+        return $this->modulo11($codigoBarra);
+    }
+
+    private function quintoCampo()
+    {
+        return $this->fatorVencimento.str_pad($this->formatarValor($this->valor), 10, "0", STR_PAD_LEFT);
     }
 
 }
